df52d5bd2c553693cdc9a2395732947ba7a2cd7d
ackampos7 <alejandrocamposfp@ibf.cat>
2023-11-30 19:21:40 +0100

    Started CSS design

--- vista/carta.php
+++ vista/carta.php
@@ -4,7 +4,7 @@
     include_once '../controlador/pedidoController.php';
     include_once '../modelo/pedidoDAO.php';
     include_once '../modelo/Producto.php';
-    include_once '../vista/header.php';
+    //include_once '../vista/header.php';
     include_once '../config/functions.php';
     
     $productos = productoDAO::getAllProducts();
@@ -21,8 +21,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="estilos.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"><link href="../css/carta.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
@@ -31,72 +30,44 @@
     <title>Restaurant Ricardo Tormo</title>
 </head>
 <body>
-    
-    <h2>Bocadillos</h2>
-    <div class="row div-productos">
+
+<section class="container-fluid">
+    <div class="row d-flex justify-content-center seccion-categoria">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6 div-categoria-titulo">
+            <h2>Bocadillos</h2>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6 div-categoria-img">
+            <img src="../imagenes/categorias/categoriabocadillos.png" alt="Imagen de la categoria de bocadillos">
+        </div>
+    </div>
+</section>
+
+<section class="container-fluid">
+    <div class="row d-flex justify-content-center seccion-productos">
     <?php
         foreach($productos as $producto) {
             if($producto->getCategoriaId() == 1) {
     ?>
-        <div class="col-12 col-md-6">
-            <img src=" <?php echo $producto->getImg() ?>" width="300" height="300" alt="">
-            <h3><?= $producto->getNombre() ?></h3>
-            <p><?= $producto->getPrecioUnidad() ?>€</p>
-            <form action="" method="post">
-                <input type="hidden" name="escondido" value="<?= $producto->getProductoId() ?>">
-                <input type="submit" name="añadircarro" value="Añadir al carrito">
-            </form>
+        <div class="col-12  col-sm-12 col-md-6 col-lg-4 div-producto">
+            <div class="div-producto-img">
+                <img src=" <?php echo $producto->getImg() ?>" alt="Imagen de bocadillo vegetal">
+            </div>
+            <div class="div-producto-detalles">
+                <h3><?= $producto->getNombre() ?></h3>
+                <p><?= $producto->getPrecioUnidad() ?>€</p>
+                <form action="" method="post">
+                    <input type="hidden" name="escondido" value="<?= $producto->getProductoId() ?>">
+                    <input type="submit" name="añadircarro" value="Añadir al carrito">
+                </form>
+            </div>
         </div>
     <?php
         }  
     }
     ?>  
     </div>
+</section>
 
-    <h2>Tapas</h2>
-    <div class="row div-productos">
-    <?php
-        foreach($productos as $producto) {
-            if($producto->getCategoriaId() == 2) {
-    ?>
-        <div class="col-12 col-md-6">
-            <img src=" <?php echo $producto->getImg() ?>" width="300" height="300" alt="">
-            <h3><?= $producto->getNombre() ?></h3>
-            <p><?= $producto->getPrecioUnidad() ?>€</p>
-            <form action="" method="post">
-                <input type="hidden" name="escondido" value="<?= $producto->getProductoId() ?>">
-                <input type="submit" name="añadircarro" value="Añadir al carrito">
-            </form>
-        </div>
-    <?php
-        }  
-    }
-    ?>  
-    </div>
-
-    <h2>Hamburguesas</h2>
-    <div class="row div-productos">
-    <?php
-        foreach($productos as $producto) {
-            if($producto->getCategoriaId() == 3) {
-    ?>
-        <div class="col-12 col-md-6">
-            <img src=" <?php echo $producto->getImg() ?>" width="300" height="300" alt="">
-            <h3><?= $producto->getNombre() ?></h3>
-            <p><?= $producto->getPrecioUnidad() ?>€</p>
-            <p> <?= $producto->getProductoId() ?> </p>
-            <form action="" method="post">
-                <input type="hidden" name="escondido" value="<?= $producto->getProductoId() ?>">
-                <input type="submit" name="añadircarro" value="Añadir al carrito">
-            </form>
-        </div>
-    <?php
-        }  
-    }
-    ?>  
-    </div>
-
-    
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
