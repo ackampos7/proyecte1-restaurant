302c8c3e5559e7fe6894f6f4f80465a6c3646716
ackampos7 <alejandrocamposfp@ibf.cat>
2023-12-04 20:16:57 +0100

    Continued CSS

--- vista/carrito.php
+++ vista/carrito.php
@@ -13,7 +13,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link href="estilos.css" rel="stylesheet">
+    <link href="../css/carrito.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&display=swap" rel="stylesheet">
@@ -22,9 +22,8 @@
     <title>Restaurant Ricardo Tormo</title>
 </head>
 <body>
-
 <?php
-    if(isset($_POST["añadircantidad"])) {
+if(isset($_POST["añadircantidad"])) {
         $idcantidad = $_POST['idescondido'];
         pedidoController::sumarCantidad($idcantidad);
         header('Location: ../vista/carrito.php');
@@ -49,43 +48,52 @@
     if(!isset($preciototal)) {
         $preciototal = 0;
     }
-
-    if(isset($_SESSION['pedido'])) {
-        foreach($_SESSION['pedido'] as $p) {
-            $productoCarro = productoDAO::getProductById($p[0]);
-            $cantidad = $p[1];
-            $precioproductototal = calcularPrecioTotal::calcularPrecioProductoTotal($cantidad, $productoCarro->getPrecioUnidad());
-            $preciototal = $preciototal + $precioproductototal;
-        ?>
-
-        <div class="col-12 producto-carrito">
-            <img src=" <?php echo $productoCarro->getImg() ?>" width="150" height="150" alt="">
-            <p><?= $productoCarro->getNombre() ?></p>
-            <form method="post">
-                <input type="hidden" name="idescondido" value="<?= $productoCarro->getProductoId() ?>">
-                <input type="hidden" name="cantidad" value="<?= $cantidad ?>">
-                <input type="submit" name="añadircantidad" value="+">        
-                <p> <?= $cantidad ?> </p>
-                <input type="submit" name="reducircantidad" value="-">
-                <input type="submit" name="borrarproductocarrito" value="X">
-                <p><?= $productoCarro->getPrecioUnidad() ?>€</p>
-                <p><?= $precioproductototal."€" ?></p>
-            </form>
-        </div>
-
-<?php
-        }
-    }
-
 ?>
-
-    <p><?= $preciototal ?>€</p>
-    <div>
-        <form method="post">
-            <input type="submit" name="finalizarpedido" value="Finalizar pedido">
-        </form>
-    </div>
-
-
+<div class="container-fluid">
+<table border="1" class="table-carrito">
+        <tr class="table-header">
+            <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Cantidad</th>
+            <th>Precio Unitario</th>
+            <th>Precio Total</th>
+            <th></th>
+        </tr>
+        <?php
+        if(isset($_SESSION['pedido'])) {
+            foreach($_SESSION['pedido'] as $p) {
+                $productoCarro = productoDAO::getProductById($p[0]);
+                $cantidad = $p[1];
+                $precioproductototal = calcularPrecioTotal::calcularPrecioProductoTotal($cantidad, $productoCarro->getPrecioUnidad());
+                $preciototal = $preciototal + $precioproductototal;
+        ?>
+        <tr class="row-producto">
+            <td><img src="<?php echo $productoCarro->getImg() ?>" height="143" width="212.83" alt=""></td>
+            <td><?= $productoCarro->getNombre() ?></td>
+            <td>
+                <form method="post" class="td-cantidad">
+                    <input type="hidden" name="idescondido" value="<?= $productoCarro->getProductoId() ?>">
+                    <input type="hidden" name="cantidad" value="<?= $cantidad ?>">
+                    <input type="submit" name="añadircantidad" value="+">
+                    <p><?= $cantidad ?></p>
+                    <input type="submit" name="reducircantidad" value="-">        
+                </form>
+            </td>
+            <td><p><?= $productoCarro->getPrecioUnidad() ?>€</p></td>
+            <td><p><?= $precioproductototal."€" ?></p></td>
+            <td>
+                <form method="post">
+                    <input type="hidden" name="idescondido" value="<?= $productoCarro->getProductoId() ?>">
+                    <input type="submit" name="borrarproductocarrito" value="X">
+                </form>
+            </td>
+        </tr>
+        <?php
+                }
+            }
+        ?>
+    </table>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
--- vista/carta.php
+++ vista/carta.php
@@ -4,7 +4,7 @@
     include_once '../controlador/pedidoController.php';
     include_once '../modelo/pedidoDAO.php';
     include_once '../modelo/Producto.php';
-    //include_once '../vista/header.php';
+    include_once '../vista/header.php';
     include_once '../config/functions.php';
     
     $productos = productoDAO::getAllProducts();
@@ -43,7 +43,7 @@
     </div>
 </section>
 
-<section class="container-fluid">
+<div class="container-fluid">
     <div class="row d-flex justify-content-center seccion-productos">
     <?php
         foreach($productos as $producto) {
@@ -67,7 +67,81 @@
     }
     ?>  
     </div>
+</div>
+
+<section class="container-fluid">
+    <div class="row d-flex justify-content-center seccion-categoria">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6 div-categoria-titulo">
+            <h2>Bocadillos</h2>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6 div-categoria-img">
+            <img src="../imagenes/categorias/categoriabocadillos.png" alt="Imagen de la categoria de bocadillos">
+        </div>
+    </div>
 </section>
+
+<div class="container-fluid">
+    <div class="row d-flex justify-content-center seccion-productos">
+    <?php
+        foreach($productos as $producto) {
+            if($producto->getCategoriaId() == 1) {
+    ?>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 div-producto">
+            <div class="div-producto-img">
+                <img src=" <?php echo $producto->getImg() ?>" alt="Imagen de bocadillo vegetal">
+            </div>
+            <div class="div-producto-detalles">
+                <h3><?= $producto->getNombre() ?></h3>
+                <p><?= $producto->getPrecioUnidad() ?>€</p>
+            </div>
+            <form action="" method="post">
+                <input type="hidden" name="escondido" value="<?= $producto->getProductoId() ?>">
+                <input class="boton-añadir" type="submit" name="añadircarro" value="Añadir al carrito">    
+            </form>
+        </div>
+    <?php
+        }  
+    }
+    ?>  
+    </div>
+</div>
+
+<section class="container-fluid">
+    <div class="row d-flex justify-content-center seccion-categoria">
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6 div-categoria-titulo">
+            <h2>Bocadillos</h2>
+        </div>
+        <div class="col-12 col-sm-12 col-md-12 col-lg-6 div-categoria-img">
+            <img src="../imagenes/categorias/categoriabocadillos.png" alt="Imagen de la categoria de bocadillos">
+        </div>
+    </div>
+</section>
+
+<div class="container-fluid">
+    <div class="row d-flex justify-content-center seccion-productos">
+    <?php
+        foreach($productos as $producto) {
+            if($producto->getCategoriaId() == 1) {
+    ?>
+        <div class="col-12 col-sm-12 col-md-6 col-lg-4 div-producto">
+            <div class="div-producto-img">
+                <img src=" <?php echo $producto->getImg() ?>" alt="Imagen de bocadillo vegetal">
+            </div>
+            <div class="div-producto-detalles">
+                <h3><?= $producto->getNombre() ?></h3>
+                <p><?= $producto->getPrecioUnidad() ?>€</p>
+            </div>
+            <form action="" method="post">
+                <input type="hidden" name="escondido" value="<?= $producto->getProductoId() ?>">
+                <input class="boton-añadir" type="submit" name="añadircarro" value="Añadir al carrito">    
+            </form>
+        </div>
+    <?php
+        }  
+    }
+    ?>  
+    </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
