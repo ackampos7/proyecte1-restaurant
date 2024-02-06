0ba8518e95eea5b30d3bd51a43238659ead10dcb
ackampos7 <alejandrocamposfp@ibf.cat>
2024-02-06 15:54:00 +0100

    Fixed QR Bug

--- src/QRGenerator.js
+++ src/QRGenerator.js
@@ -1,4 +1,5 @@
 let pedidoid = document.getElementById("pedidoid").value;
 localStorage.setItem("pedidoid", pedidoid);
+let encodedid = encodeURIComponent(pedidoid);
 qrIMG = document.getElementById("qrimg");
-qrIMG.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=http://localhost/proyecte1-restaurant/?controller=pedido&action=infoPedido&pedidoid=${pedidoid}`;
+qrIMG.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(`http://localhost/proyecte1-restaurant/?controller=pedido&action=infoPedido&pedidoid=${encodedid}`);
